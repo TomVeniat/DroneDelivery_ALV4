7e2f0e05b3f154017a670e10ba455600600161b7
Marc Karassev <marc.karassev@yahoo.fr>
2015-10-17 18:44:55 +0200

    kafka producer consumer nodejs example

---
+++ node/kafka_producer_consumer_example/consumer.js
@@ -0,0 +1,28 @@
+/**
+	@author Marc Karassev
+
+	Script creating a kafka consumer consuming all messages from the beginning from 2 topics during 5 seconds before terminating.
+*/
+
+console.log("requires a kafka server running with default configuration");
+
+var kafka = require("kafka-node"),
+	Consumer = kafka.Consumer,
+	client = new kafka.Client(),
+	marc = new Consumer(
+		client,
+		[{topic: "expose"}, {topic: "projet"}],
+		{autocommit: false, fromOffset: true}
+	);
+
+marc.on('message', function (message) {
+	console.log(message);
+});
+
+marc.on("error", function (err) {
+	console.log(err);
+});
+
+setTimeout(function () {
+	marc.close();
+}, 5000);
