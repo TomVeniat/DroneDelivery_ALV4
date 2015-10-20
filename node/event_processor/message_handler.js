a593bff6fe7973accd593dcc35595d6351dbdf9f
Marc Karassev <marc.karassev@greencom-networks.com>
2015-10-20 19:26:57 +0200

    added body-handler dependency logging body

--- node/event_processor/message_handler.js
+++ node/event_processor/message_handler.js
@@ -1,10 +1,13 @@
 var express = require("express"),
+	bodyParser = require("body-parser"),
 	app = express();
+
+app.use(bodyParser.json());
 
 app.post('/drone_message', function (req, res) {
 	var message = req.body;
 
-	console.log(req);
+	console.log(message);
 	res.end('On a reçu ton POST');
 });
 
