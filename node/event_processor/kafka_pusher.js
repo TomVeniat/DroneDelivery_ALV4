691369e977517b43ce8b857185f9f7a9b79204b6
Marc Karassev <marc.karassev@greencom-networks.com>
2015-10-23 10:03:27 +0200

    MàJ configuration serveur distant

--- node/event_processor/kafka_pusher.js
+++ node/event_processor/kafka_pusher.js
@@ -4,7 +4,7 @@
 
 var kafka = require("kafka-node"),
 	Producer = kafka.Producer,
-	client = new kafka.Client("37.187.126.101:2181"),
+	client = new kafka.Client("37.187.126.101:8082"),
 	producer = new Producer(client)
 
 producer.on("ready", function () {
