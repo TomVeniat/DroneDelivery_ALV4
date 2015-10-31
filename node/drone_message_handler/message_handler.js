3370c18c44942864e630af52d38b5ab82a88e8e3
Marc Karassev <marc.karassev@yahoo.fr>
2015-10-31 19:27:00 +0100

    MàJ de commentaires et package.json

--- node/drone_message_handler/message_handler.js
+++ node/drone_message_handler/message_handler.js
@@ -6,15 +6,24 @@ var express = require("express"),
 	router = express.Router(),
 	kafkaPusher = require("./kafka_pusher");
 
+/*
+	Awaits for POST requests with body containing a JSON object
+	representing a drone message like the following:
+	{
+		"id": 0,
+		"lat": 10.1,
+		"lon": 12.1,
+		"alt": 15.0,
+		"fuel": 99,
+		"event": "usual_tracking_message"
+	}
+*/
 router.post('/drone_message', function (req, res) {
 	var message = req.body;
+	
 	console.log(message);
 	// TODO check message validity
-//	trackingProcessor.processTracking(message); ancien code
-
-	// FORME DU JSON : {"id":"0","event":"drone","deliveryNumber":"0"}
 	kafkaPusher.pushEvent(message);
-	
 	res.end();
 });
 
