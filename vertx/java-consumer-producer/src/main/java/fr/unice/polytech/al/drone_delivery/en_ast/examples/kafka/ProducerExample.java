bfa005d1e67fd8302751a07c11a6334e1c2105ef
Marc Karassev <marc.karassev@yahoo.fr>
2016-01-05 23:04:22 +0100

    copy/pasted producer

---
+++ vertx/java-consumer-producer/src/main/java/fr/unice/polytech/al/drone_delivery/en_ast/examples/kafka/ProducerExample.java
@@ -0,0 +1,31 @@
+package fr.unice.polytech.al.drone_delivery.en_ast.examples.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * @author Marc Karassev
+ */
+public class ProducerExample {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:4242");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer(props);
+        for(int i = 0; i < 100; i++)
+            producer.send(new ProducerRecord<String, String>("my-topic", Integer.toString(i), Integer.toString(i)));
+
+        producer.close();
+    }
+}
