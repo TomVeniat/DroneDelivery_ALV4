2dfebe2362dfa451ba2c7c8d9b1783f3b27cc6e1
Marc Karassev <marc.karassev@yahoo.fr>
2016-01-05 21:37:36 +0100

    VertX web HelloWorld

---
+++ vertx/kafka-producer-consumer-vertx/src/main/java/fr/unice/polytech/al/drone_delivery/en_ast/kafka_examples/Server.java
@@ -0,0 +1,21 @@
+package fr.unice.polytech.al.drone_delivery.en_ast.kafka_examples;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
+
+/**
+ * @author Marc Karassev
+ */
+public class Server extends AbstractVerticle {
+
+    @Override
+    public void start() throws Exception {
+
+        Router router = Router.router(vertx);
+
+        router.route().handler(routingContext -> routingContext.response().putHeader("content-type", "text/html").end("Hello World!"));
+
+        vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+    }
+}
+
