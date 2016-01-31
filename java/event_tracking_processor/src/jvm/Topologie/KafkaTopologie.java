d8a74056c2262956120d201d43cdf163342b3457
quentin <qcornevin@gmail.com>
2016-01-31 14:50:04 +0100

    Normalement maintenant c'est propre

---
+++ java/event_tracking_processor/src/jvm/Topologie/KafkaTopologie.java
@@ -0,0 +1,48 @@
+package Topologie;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.spout.SchemeAsMultiScheme;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.utils.Utils;
+import storm.kafka.BrokerHosts;
+
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
+import storm.kafka.ZkHosts;
+import storm.kafka.bolt.KafkaBolt;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by jinhong on 29/01/2016.
+ */
+public class KafkaTopologie {
+    public static void main(String[] args) throws Exception {
+        BrokerHosts brokerHosts = new ZkHosts("localhost:2181");
+        SpoutConfig spoutConfig = new SpoutConfig(brokerHosts, "inputTopic", "/zkkafkaspout" , "kafkaspout");
+        Config conf = new Config();
+        Map<String, String> map = new HashMap<>();
+        map.put("metadata.broker.list", "localhost:9092");
+        map.put("serializer.class", "kafka.serializer.StringEncoder");
+        conf.put("kafka.broker.properties", map);
+        conf.put("topic", "topic2");
+        spoutConfig.scheme = new SchemeAsMultiScheme(new MessageScheme());
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("spout", new KafkaSpout(spoutConfig));
+        builder.setBolt("bolt", new SenquenceBolt()).shuffleGrouping("spout");
+        builder.setBolt("kafkabolt", new KafkaBolt<String, Integer>()).shuffleGrouping("bolt");
+        if (args != null && args.length > 0) {
+            conf.setNumWorkers(3);
+            StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+        }else{
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("Topo", conf, builder.createTopology());
+            Utils.sleep(10000);
+            cluster.killTopology("Topo");
+            cluster.shutdown();
+        }
+    }
+}
---
+++ java/topology_sandbox/src/main/java/TestTopology.java
@@ -0,0 +1,49 @@
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.spout.SchemeAsMultiScheme;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.utils.Utils;
+import storm.kafka.*;
+
+import java.util.UUID;
+
+/**
+ * @author Marc Karassev
+ */
+public class TestTopology {
+
+    public static void main(String[] args) {
+        BrokerHosts hosts = new ZkHosts("127.0.0.1:2181");
+        SpoutConfig spoutConfig = new SpoutConfig(hosts, "test", "/" + "test", UUID.randomUUID().toString());
+        spoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
+        KafkaSpout kafkaSpout = new KafkaSpout(spoutConfig);
+
+//        KafkaBolt kafkaBolt= new KafkaBolt()
+//                .withTopicSelector(new DefaultTopicSelector("out"))
+//                .withTupleToKafkaMapper(new FieldNameBasedTupleToKafkaMapper());
+
+        TopologyBuilder builder = new TopologyBuilder();
+
+        builder.setSpout("kafka spout", kafkaSpout);
+        //builder.setSpout("kafka spout", new KafkaSpout());
+        builder.setBolt("bolt", new TestBolt())
+                .shuffleGrouping("kafka spout");
+//        builder.setBolt("kafka bolt", kafkaBolt)
+//                .shuffleGrouping("kafka spout");
+
+        Config config = new Config();
+        config.setDebug(false);
+        //Properties props = new Properties();
+//        props.put("metadata.broker.list", "localhost:2181");
+//        props.put("request.required.acks", "1");
+//        props.put("serializer.class", "kafka.serializer.StringEncoder");
+//        props.put("topic", "out");
+//        config.put(KafkaBolt.KAFKA_BROKER_PROPERTIES, props);
+
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("test", config, builder.createTopology());
+        Utils.sleep(10000);
+        cluster.killTopology("test");
+        cluster.shutdown();
+    }
+}
